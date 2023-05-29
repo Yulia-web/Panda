7d1128bc760f95db5f5179c42b1b99356f878a6f
Linzy <lyashchukyulia@gmail.com>
2023-05-29 09:39:40 +0300

    Added styles for header

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as flsFunction from "./modules/function.js";
 import * as burger from "./menu/menu_burger.js";
+import * as gallery from "./modules/gallery.js";
 
 flsFunction.isWebp();
 burger.burgerMenu();
+gallery.gallery();
     
